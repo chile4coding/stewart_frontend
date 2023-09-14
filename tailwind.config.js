6b65fd013a1dbcb03fdf33186215200d09261557
chile4coding <chileomereji@gmail.com>
2023-09-14 08:01:22 +0100

    added boiler plate

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: { min: "0px", max: "640px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "641px", max: "768px" },
+      // md: { min: "768px", max: "1023px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "769px", max: "1440px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+      xl: { min: "1441px" },
+      // => @media (min-width: 1536px) { ...
+    },
+    fontFamily: {
+      Mukta: "Mukta",
+    },
+
+    extend: {
+      colors: {
+        accent: {
+          100: "#3188FF",
+        },
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "white", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
+};
