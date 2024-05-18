75ad8d4fb451c7c0a015cde0b1f7089b52447ea5
chile4coding <chileomereji@gmail.com>
2024-05-18 14:30:13 +0100

    fix now

--- components/admin/auth/Login.js
+++ components/admin/auth/Login.js
@@ -51,7 +51,7 @@ export default function AdminLogin() {
 
       dispatch(setAdmin(data?.findAdminUpdate));
       Cookies.set("_stewart_collection_token", data.token);
-      window.location.href = "/admin/products";
+      window.location.href = "/admin/home";
     } else {
       toast.error(<div className=" normal-case">{data?.message}</div>);
     }
